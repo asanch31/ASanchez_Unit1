2df438061b676b6d91e1f221ea990707610f1d1d
asanch31 <78468358+asanch31@users.noreply.github.com>
2021-02-13 15:40:38 -0800

    Lesson C 1.0
    Added public speed variable for player to control
    Created new script to allow camera to follow camera, with offset to position camera properly

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //intialize float to allow user control of car(object) speed.
+    public float speed = 15.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime*10);
+        transform.Translate(Vector3.forward * Time.deltaTime*speed);
     }
 }
---
+++ Assets/Scripts/playerCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCamera : MonoBehaviour
+{
+    //allow object to connect with player object
+    public GameObject player;
+    // offset camera from player position (third person view)
+    private Vector3 offset = new Vector3(0, 4, -12);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //allow camera to follow object with offset for third person view
+        transform.position = player.transform.position + offset;
+    }
+}
